fcf79ca20a017fbb1f6d4082f04652372730d7e6
matteobonelli <matteobonelli08@gmail.com>
2023-10-17 15:14:04 +0200

    script divided in many functions

--- js/script.js
+++ js/script.js
@@ -1,33 +1,50 @@
-const btn = document.querySelector('button');
 
-btn.addEventListener('click', function(){
-    const inputWord = document.getElementById('data').value;
-    console.log(inputWord);
-    let letterPalindromo;
-    let container = [];
-    let message;
-    let alertColor;
-    if(inputWord === ''){
-        message = 'Scrivi qualcosa di valido!';
-        alertColor = 'alert-warning';
-    } else{
-        for(i = inputWord.length - 1; i >= 0; i--){
-            letterPalindromo = inputWord[i];
-            container.push(letterPalindromo);
-        }
-        let palindromo = container.join('');
-        console.log(palindromo);
-        if(palindromo === inputWord){
-            message = 'Questa parola è un palindromo!';
-            alertColor = 'alert-success';
+(function checkIfPalindromo(){
+    const btn = document.querySelector('button');
+
+    btn.addEventListener('click', ifPalindromo);
+
+    function ifPalindromo(){
+        const inputWord = document.getElementById('data').value;
+        removeClassesAlert();
+        console.log(inputWord);
+        let container = [];
+        let message;
+        let alertColor;
+        if(inputWord === ''){
+            message = 'Scrivi qualcosa di valido!';
+            alertColor = 'alert-warning';
         } else{
-            message = 'Questa parola non è palindroma!';
-            alertColor = 'alert-danger';
+            for(i = inputWord.length - 1; i >= 0; i--){
+                let letterPalindromo = inputWord[i];
+                container.push(letterPalindromo);
+            }
+            let palindromo = container.join('');
+            console.log(palindromo);
+            if(palindromo === inputWord){
+                message = 'Questa parola è un palindromo!';
+                alertColor = 'alert-success';
+            } else{
+                message = 'Questa parola non è palindroma!';
+                alertColor = 'alert-danger';
+            }
         }
+        printOutputAlert(message, alertColor);
+    };
+
+    function printOutputAlert (msg, aColor){
+        const outputMessage = document.querySelector('.alert');
+        outputMessage.innerText = msg;
+        outputMessage.classList.add(aColor);
+        outputMessage.classList.remove('d-none');
     }
-    const outputMessage = document.querySelector('.alert');
-    outputMessage.className = 'alert d-none'
-    outputMessage.innerText = message;
-    outputMessage.classList.add(alertColor);
-    outputMessage.classList.remove('d-none');
-})
+
+    function removeClassesAlert(){
+        document.getElementById('data').value = '';
+        const outputMessage = document.querySelector('.alert');
+        outputMessage.classList.remove('alert-warning');
+        outputMessage.classList.remove('alert-success');
+        outputMessage.classList.remove('alert-danger');
+    }
+})();
+
